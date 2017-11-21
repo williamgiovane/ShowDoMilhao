fffd2f7f8a484ca64101b03f96c18a5c9c717ee8
William Giovani Testoni <williamgiovane1@gmail.com>
2017-11-21 19:04:13 -0200

    [IMP] Implementado arquivos

---
+++ src/showdomilhao/ShowDoMilhao.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package showdomilhao;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Willi
+ */
+public class ShowDoMilhao {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame menu = new Menu();
+        
+        menu.setVisible(true);
+    }
+    
+}
